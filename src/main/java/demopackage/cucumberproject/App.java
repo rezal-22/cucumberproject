fc187306b97cc7579d00cd3b1850b57cb4c6b0be
rohit-sanker250 <rohitsanker14@gmail.com>
2024-11-19 13:23:20 +0530

    1st commit

---
+++ src/main/java/demopackage/cucumberproject/App.java
@@ -0,0 +1,13 @@
+package demopackage.cucumberproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
